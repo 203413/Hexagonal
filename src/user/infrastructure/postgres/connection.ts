8a01ea62acdcefd21f872331a16bb30da4c2d9ba
AvrlFstr <77992655+203413@users.noreply.github.com>
2023-05-27 01:18:40 -0600

    feat(Hexagonal): aws rds url added

--- src/user/infrastructure/postgres/connection.ts
+++ src/user/infrastructure/postgres/connection.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   user: "postgres",
-  host: "localhost",
+  host: "db-test-instance-pls-ignore.c3sf0mwmifqf.us-east-1.rds.amazonaws.com",
   password: "greggrulzok",
   database: "dbprueba",
   port: 5432,
